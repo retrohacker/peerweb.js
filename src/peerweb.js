30c5b53a81cecd80e89c7f2e911120a9ec11f9e4
Garox <gabriel@garox.org>
2017-12-23 17:08:21 -0300

    working

--- src/peerweb.js
+++ src/peerweb.js
@@ -21,46 +21,35 @@ function renderFromTorrent (torrent) {
   const index = torrent.files.find(function (file) {
     return file.name.endsWith('index.html')
   })
-  console.log('termino')
-  console.log(index)
-  console.log(torrent.dn.length)
+  let replaceObject = {}
   // Store each file in browser storage
   async.each(torrent.files, function (file, cb) {
     file.getBlobURL(function (e, bloburl) {
       if (e && peerweb.debug) console.log('Failed to get blob for', file.path, ':', e)
       if (e) return null
-      console.log(bloburl)
       let path  = file.path.slice(torrent.dn.length + 1)
       if (peerweb.debug) console.log('Adding', path, 'to browser storage')
-      localforage.setItem(path, bloburl).then(function () { cb() }).catch(function (e) { cb(e) })
+      localforage.setItem(path, bloburl)
+      .then(() => {
+        replaceObject[path] = bloburl
+        cb()
+      })
+      .catch(cb)
     })
   }, function (e) {
     if (e && peerweb.debug) console.log('Failed to add files to local storage', e)
     if (e) return null
-    console.log('hecho')
+    console.log(replaceObject)
     index.getBuffer(function (e, buffer) {
       if (e && peerweb.debug) console.log('Failed to get index.html buffer', e)
       if (e) return null
       let stringHTML = buffer.toString()
-      localforage.getItem("pulpitrock.jpg")
-      .then(function(readValue) {
-        console.log('Read: ', readValue);
-        return stringHTML.replaceAll('pulpitrock.jpg', readValue)
-      })
-      .then((h)=> document.body.innerHTML = h)
-      
+      for (let key in replaceObject) {
+        stringHTML = stringHTML.replaceAll(key, replaceObject[key])
+      }
+      document.body.innerHTML = stringHTML
     })
   })
 }
 
-peerweb.init = function peerwebInit (cb) {
-  cb()
-}
-
-peerweb.init(function (e) {
-  if(e) throw e
-  peerweb.debug = true
-  peerweb.render('magnet:?xt=urn:btih:ceeaced1aca41e46b23c595c46d8688fcb62070a&dn=page&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com')
-})
-
-console.log('iniciando')
+peerweb.render('magnet:?xt=urn:btih:760682a76de62478622298117f60fc39f7784cf6&dn=page&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com')
