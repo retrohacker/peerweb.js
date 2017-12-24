79d3e85c1dce2dccbd378de5c2c87eee5e8beba4
Garox <gabriel@garox.org>
2017-12-24 04:16:40 -0300

    npm

--- src/peerweb.js
+++ src/peerweb.js
@@ -1,10 +1,14 @@
 import WebTorrent from './webtorrent' // or 'webtorrent/webtorrent.min'
 import async from 'async'
 
+
 /* Mutation */
 String.prototype.replaceAll = function (search, replacement) { return this.replace(new RegExp(search, 'g'), replacement) }
 // Initalize WebTorrent
 const client = new WebTorrent()
+// This is shorter
+const { log } = console
+
 // Create class to export
 export default class peerweb {
   constructor (debug = false) {
@@ -12,7 +16,7 @@ export default class peerweb {
   }
   render (magnet) {
     const that = this
-    if (peerweb.debug) console.log('Downloading torrent from', magnet)
+    if (peerweb.debug) log('Downloading torrent from', magnet)
     client.add(magnet, function (torrent) {
       renderFromTorrent(torrent, that)
     })
@@ -20,8 +24,8 @@ export default class peerweb {
 }
 
 function renderFromTorrent(torrent, peerweb) {
-  if (peerweb.debug) console.log('Torrent Downloaded:')
-  if (peerweb.debug) console.log(torrent)
+  if (peerweb.debug) log('Torrent Downloaded:')
+  if (peerweb.debug) log(torrent)
   // Get index.html
   const index = torrent.files.find(function (file) {
     return file.name.endsWith('index.html')
@@ -31,17 +35,19 @@ function renderFromTorrent(torrent, peerweb) {
   async.each(torrent.files, function (file, cb) {
     file.getBlobURL(function (e, bloburl) {
       if (e && peerweb.debug)
-        console.log('Failed to get blob for', file.path, ':', e)
+        log('Failed to get blob for', file.path, ':', e)
       if (e) return null
       let path = file.path.slice(torrent.dn.length + 1)
-      if (peerweb.debug) console.log('Adding', path, 'to browser storage')
+      if (peerweb.debug) log('Adding', path, 'to browser storage')
       replaceObject[path] = bloburl
+      cb() // not sure if needed
     })
   }, function (e) {
-    if (e && peerweb.debug) console.log('Failed to add files to local storage', e)
+    if (e && peerweb.debug) log('Failed to add files to local storage', e)
     if (e) return null
+    if (peerweb.debug) log('Files added, will render index soon')
     index.getBuffer(function (e, buffer) {
-      if (e && peerweb.debug) console.log('Failed to get index.html buffer', e)
+      if (e && peerweb.debug) log('Failed to get index.html buffer', e)
       if (e) return null
       let stringHTML = buffer.toString();
       for (let key in replaceObject) stringHTML = stringHTML.replaceAll(key, replaceObject[key])
