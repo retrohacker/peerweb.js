4972b7b9f3b012178319f5642dcc8c092ab2d452
Garox <gabriel@garox.org>
2018-02-04 19:34:51 -0300

    working

--- src/peerweb.js
+++ src/peerweb.js
@@ -1,5 +1,6 @@
 import WebTorrent from './webtorrent' // or 'webtorrent/webtorrent.min'
 import async from 'async'
+var Buffer = require('buffer/').Buffer 
 
 /* Mutation */
 String.prototype.replaceAll = function (search, replacement) { return this.replace(new RegExp(search, 'g'), replacement) }
@@ -7,35 +8,67 @@ String.prototype.replaceAll = function (search, replacement) { return this.repla
 const client = new WebTorrent()
 // This is shorter
 const { log, info } = console
-
+const announce = ['wss://tracker.fastcast.nz',
+'wss://tracker.openwebtorrent.com',
+'wss://tracker.btorrent.xyz'
+]
 // Create class to export
-export default class peerweb {
-  constructor (debug = false) {
+export default class Peerweb {
+  constructor (debug = false, name = 'defaultapp') {
     this.d = debug
+    this.hash = new Buffer(20).fill(name)
+    this.peers = []
+    this._discoverPeers()
+    this.sites = []
+    this.publish = this.publish.bind(this)
+    this.render = this.render.bind(this)
   }
   debug (text) {
-    if (this.d) log(text)
+    if (this.d) info(text)
+  }
+  publish (page) {
+    this.broadcast({addSite: page})
   }
   render (magnet) {
-    const that = this
     this.debug('Downloading torrent from ' + magnet)
-    client.add(magnet, function (torrent) {
-      renderFromTorrent(torrent, that)
+    client.add(magnet, torrent => {
+      //renderFromTorrent(torrent, this)
+      this.publish({name:'test', magnetURI: magnet})
     })
   }
-  getMagnet (files) {
+  getMagnet (name, files) {
     const debug = this.debug.bind(this)
     return new Promise((resolve,reject) => {
       client.seed(files, torrent => {
         torrent.on('error', reject)
         debug(torrent)
-        resolve(torrent.magnetURI)
+        const { magnetURI } = torrent 
+        this.publish({name, magnetURI})
+        resolve(magnetURI)
       })
     })
   }
+  broadcast (obj) {
+    this.peers.forEach(function (peer) {
+      if (peer.connected) peer.send(JSON.stringify(obj))
+    })
+  }
+  _getTracker (opts) {
+    return new client.Discovery(opts)
+  }
+  get tracker () {
+    return this._getTracker({
+      infoHash: this.hash,
+      peerId: client.peerId,
+      announce
+    })
+  }
+  _discoverPeers () {
+    this.tracker.on('peer', peer => onPeer(peer, this))
+  }
 }
 
-function renderFromTorrent(torrent, peerweb) {
+function renderFromTorrent (torrent, peerweb) {
   peerweb.debug('Torrent Downloaded:')
   peerweb.debug(torrent)
   // Get index.html
@@ -68,13 +101,13 @@ function renderFromTorrent(torrent, peerweb) {
   })
 }
 
-function renderHTML(buffer, replaceObject) {
+function renderHTML (buffer, replaceObject) {
   let stringHTML = buffer.toString()
   for (let key in replaceObject) stringHTML = stringHTML.replaceAll(key, replaceObject[key])
   document.documentElement.innerHTML = stringHTML
 }
 
-function evaluateJS() {
+function evaluateJS () {
   let scripts = document.getElementsByTagName('script')
   const { length } = scripts
   for (let i = 0; i < length; i++) { // Can't use .map because it's a HTMLCollection
@@ -84,4 +117,83 @@ function evaluateJS() {
     document.body.appendChild(scrpt)
     scripts[i].parentElement.removeChild(scripts[i]) // Remove duplication
   }
+}
+
+function onPeer (peer, self) {
+  let { peers, sites, broadcast} = self
+  if (included(peers, peer)) return undefined
+  peers.push(peer)
+  if (peer.connected) onConnect()
+  else peer.once('connect', onConnect)
+  function onConnect () {
+    peer.on('data', onMessage)
+    peer.on('close', onClose)
+    peer.on('error', onClose)
+    peer.on('end', onClose)
+    if (sites == []) {
+      broadcast({ isEmpty: true})
+    }
+  }
+  function onClose () {
+    peer.removeListener('data', onMessage)
+    peer.removeListener('close', onClose)
+    peer.removeListener('error', onClose)
+    peer.removeListener('end', onClose)
+    peers.splice(peers.indexOf(peer), 1)
+  }
+
+  function onMessage (data) {
+    try {
+      data = JSON.parse(data)
+    } catch (err) {
+      console.error(err.message)
+    }
+    if (data.isEmpty && sites != []) {
+      peer.send(JSON.stringify({ addSite: sites }))
+    }
+    if (sites == [] && data.sites) {
+      sites = data.sites
+    }
+    if (data.addSite) {
+      sites.push(data.addSite)
+    }
+  }
+
+  /*
+  if (peer.connected) onConnect()
+  else peer.once('connect', onConnect)
+  function onConnect () {
+    peer.on('data', onMessage)
+    peer.on('close', onClose)
+    peer.on('error', onClose)
+    peer.on('end', onClose)
+    peer.send(JSON.stringify({ page }))
+  
+    function onClose () {
+      peer.removeListener('data', onMessage)
+      peer.removeListener('close', onClose)
+      peer.removeListener('error', onClose)
+      peer.removeListener('end', onClose)
+      peers.splice(peers.indexOf(peer), 1)
+    }
+  
+    function onMessage (data) {
+      try {
+        data = JSON.parse(data)
+      } catch (err) {
+        console.error(err.message)
+      }
+      log(data)
+    }
+  }
+ */
+}
+
+
+function included (peers, peer) {
+  let bol = false
+  peers.map(p => {
+    if (p.id == peer.id) bol = true
+  })
+  return bol
 }
